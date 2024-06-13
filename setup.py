b6628763cbf1b83f1866ed96506d44ae403c7749
Gonçalo Branquinho <goncalobranquinhom@gmail.com>
2024-06-13 21:32:59 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='typerush',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    dependency_links=[
+        'git+https://github.com/GoncaloBranquinho/typerRush/#egg=typerush-1.0',
+    ],
+    install_requires=[
+        'curses-menu', 
+    ],
+    package_data={ '': ['sentences.json']},
+    entry_points={
+        'console_scripts': [
+            'typerush = typing_1:main',  
+        ],
+    },
+)
